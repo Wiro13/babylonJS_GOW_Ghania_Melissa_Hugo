685d6c222de07e6cc626dfcf5abc92b0ef2d5fe9
ghania <benmezianeghania137@gmail.com>
2024-03-29 15:42:41 +0000

    factorisation de model et ajout skieur ,sol,et ajout des methodes

---
+++ src/Models.js
@@ -0,0 +1,65 @@
+import { Color3, Color4, CubeTexture, DefaultRenderingPipeline, DirectionalLight, FlyCamera, FollowCamera, FreeCamera, GizmoManager, HavokPlugin, HemisphericLight, KeyboardEventTypes, MeshBuilder, MotionBlurPostProcess, ParticleSystem, PhysicsAggregate, PhysicsMotionType, PhysicsShapeType, Quaternion, Scalar, Scene, SceneLoader, ShadowGenerator, Sound, StandardMaterial, TargetCamera, Texture, TransformNode, UniversalCamera, Vector3 } from "@babylonjs/core";
+import { Inspector } from "@babylonjs/inspector";
+import HavokPhysics from "@babylonjs/havok";
+
+
+import mountainUrl from "../assets/models/snowy_slope.glb";
+import skierUrl from "../assets/models/skier_lowpoly.glb";
+
+export class Models{
+
+    constructor(scene){
+        this.scene=scene;
+    }
+
+
+    importMontain(){
+ 
+        SceneLoader.ImportMeshAsync("", "",mountainUrl).then((result) => {
+            let mountain = result.meshes[1];
+            mountain.name ="Moutain"
+            mountain.position = new Vector3(0, 0, 0);
+            mountain.scaling = new Vector3(10, 10, 10);
+    
+            let mountainMaterial = new StandardMaterial("mountainMaterial",this.scene);
+            mountainMaterial.diffuseColor = new Color3(1, 1, 1);
+            mountain.material = mountainMaterial;
+    
+            new PhysicsAggregate(mountain, PhysicsShapeType.MESH, { mass: 0, restitution: 0 }, this.scene);
+        });
+    
+    }
+
+
+    skieur(){
+        SceneLoader.ImportMeshAsync("", "", skierUrl,).then((result) => {
+            let capsule = MeshBuilder.CreateBox("collider", this.scene);
+            capsule.material = new StandardMaterial("capsuleMat");
+            capsule.isVisible = false;
+            let skier = result.meshes[1]
+            skier.parent = capsule;
+            skier.scaling = new Vector3(0.01, 0.01, 0.01);
+
+            capsule.position.y = 5;
+            capsule.position.x = 3.5;
+    
+            new PhysicsAggregate(capsule, PhysicsShapeType.CONVEX_HULL, { mass: 3, restitution: 0 },this.scene);
+        });
+    }
+
+    ground(){
+        //creer le ground
+        let ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, this.scene);
+        //gere la physique
+        const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, this.scene);
+        //gere la couleur 
+        let snowMaterial = new StandardMaterial("snowMaterial", this.scene);
+        snowMaterial.diffuseColor = new Color3(0.8, 0.8, 1); // Light blue color
+        snowMaterial.specularColor = new Color3(0, 0, 0);
+        ground.material = snowMaterial;
+         
+    }
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { Engine, ArcFollowCamera, ArcRotateCamera, BoundingInfo, Color3, Color4, CubeTexture, DefaultRenderingPipeline, DirectionalLight, FlyCamera, FollowCamera, FreeCamera, GizmoManager, HavokPlugin, HemisphericLight, KeyboardEventTypes, MeshBuilder, MotionBlurPostProcess, ParticleSystem, PhysicsAggregate, PhysicsMotionType, PhysicsShapeType, Quaternion, Scalar, Scene, SceneLoader, ShadowGenerator, Sound, StandardMaterial, TargetCamera, Texture, TransformNode, UniversalCamera, Vector3 } from "@babylonjs/core";
 import { Inspector } from "@babylonjs/inspector";
 import HavokPhysics from "@babylonjs/havok";
+import { Models } from "./Models";
 
 import meshUrl from "../assets/models/skier_lowpoly.glb";
 import mountainUrl from "../assets/models/snowy_slope.glb";
-import skierUrl from "../assets/models/skier_lowpoly.glb";
+
 
 let engine, canvas, papa, camera;
 
@@ -54,19 +55,10 @@ var createScene = async () => {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape.
-    let sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+   
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.x = 10;
 
-    // Our built-in 'ground' shape.
-    let ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
-    let snowMaterial = new StandardMaterial("snowMaterial", scene);
-    snowMaterial.diffuseColor = new Color3(0.8, 0.8, 1); // Light blue color
-    snowMaterial.specularColor = new Color3(0, 0, 0);
-    ground.material = snowMaterial;
 
     const havokInstance = await HavokPhysics();
     const hk = new HavokPlugin(true, havokInstance);
@@ -75,43 +67,20 @@ var createScene = async () => {
     papa = MeshBuilder.CreateCapsule("papa",scene);
     papa.position = new Vector3(0,5,0);
 
-    const sphereAggregate = new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1, restitution: 0.75 }, scene);
-    const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
+    
+    
     const papaAggregate = new PhysicsAggregate(papa, PhysicsShapeType.CAPSULE, { mass: 1, restitution: 0.75 }, scene);
+    
+    /**************************************Gestion des Object 3D******************************************/
+    var ground = new Models(scene);
+    ground.ground();
+    
+    var map = new Models(scene);
+    //map.importMontain();
 
-    SceneLoader.ImportMeshAsync("", "", skierUrl,).then((result) => {
-        // Create a collider box
-        let capsule = MeshBuilder.CreateBox("collider", scene);
-        capsule.material = new StandardMaterial("capsuleMat");
-        capsule.isVisible = false;
-        let skier = result.meshes[1]
-        skier.parent = capsule;
-        skier.scaling = new Vector3(0.01, 0.01, 0.01);
-        // camera.lockedTarget = capsule;
-
-
-        capsule.position.y = 5;
-        capsule.position.x = 3.5;
-
-        new PhysicsAggregate(capsule, PhysicsShapeType.CONVEX_HULL, { mass: 3, restitution: 0 }, scene);
-    });
-    /*
-    // Importation et application de la physique au mesh de montagne
-    SceneLoader.ImportMeshAsync("", "", mountainUrl).then((result) => {
-        let mountain = result.meshes[1];
-        mountain.name ="Moutain"
-        mountain.position = new Vector3(0, 0, 0);
-        mountain.scaling = new Vector3(10, 10, 10);
-
-        let mountainMaterial = new StandardMaterial("mountainMaterial", scene);
-        mountainMaterial.diffuseColor = new Color3(1, 1, 1);
-        mountain.material = mountainMaterial;
-
-        //new PhysicsAggregate(mountain, PhysicsShapeType.BOX, { mass: 0, restitution: 0 }, scene);
-        new PhysicsAggregate(mountain, PhysicsShapeType.MESH, { mass: 0, restitution: 0 }, scene);
-    });
-    */
-   //importMap(scene);
+    var skieur = new Models(scene);
+    //skieur.skieur();
+   
     return scene;
 };
 
@@ -154,20 +123,3 @@ function updateRotation() {
     papa.rotation.y = angle;
 }
 
-function importMap(scene){
- 
-    SceneLoader.ImportMeshAsync("", "", mountainUrl).then((result) => {
-        let mountain = result.meshes[1];
-        mountain.name ="Moutain"
-        mountain.position = new Vector3(0, 0, 0);
-        mountain.scaling = new Vector3(10, 10, 10);
-
-        let mountainMaterial = new StandardMaterial("mountainMaterial", scene);
-        mountainMaterial.diffuseColor = new Color3(1, 1, 1);
-        mountain.material = mountainMaterial;
-
-        //new PhysicsAggregate(mountain, PhysicsShapeType.BOX, { mass: 0, restitution: 0 }, scene);
-        new PhysicsAggregate(mountain, PhysicsShapeType.MESH, { mass: 0, restitution: 0 }, scene);
-    });
-
-}
